afc7783a80f08b01592edf446b99e65c2e144c59
DmitryChitalov <cdi9@yandex.ru>
2023-12-29 19:28:09 +0500

    !

--- hh_parser/web_app/hh_flask_parser.py
+++ hh_parser/web_app/hh_flask_parser.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.join(os.getcwd(), '..'))
 
 import multiprocessing as ml
-from hh_parser.parser_app import main as pr
+from parser_app import main as pr
 from hh_parser.web_app import create_app
 
 if __name__ == "__main__":
