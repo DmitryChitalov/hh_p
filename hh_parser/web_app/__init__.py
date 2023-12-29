6ed6a312ac23965711bcb82ff3c293dbbdbacefd
DmitryChitalov <cdi9@yandex.ru>
2023-12-29 18:10:24 +0500

    !

---
+++ hh_parser/web_app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+SECRET_KEY = b'\x143#\x1eV;\xc9\xa0\xecr\r\xd4/{b\n'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+def create_app():
+    from web_app.flask_parser.flask_parser import parser_blueprint
+    from web_app.authorization.auth import auth_blueprint
+    app.register_blueprint(parser_blueprint)
+    app.register_blueprint(auth_blueprint)
+
+    return app
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
---
+++ hh_parser/web_app/hh_flask_parser.py
@@ -0,0 +1,16 @@
+
+import multiprocessing as ml
+from parser_app import main as pr
+from web_app import create_app
+
+if __name__ == "__main__":
+    # Создать приложение Flask
+    app = create_app()
+
+    # Запустить парсер
+    par_service = ml.Process(name="HH Parser", target=pr.main)
+    par_service.start()
+
+
+    # Запустить Flask приложение
+    app.run()
